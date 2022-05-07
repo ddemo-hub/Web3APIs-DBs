382d0a72dde676afbdb52e775c03ce287c707d04
dem0-0 <85748316+dem0-0@users.noreply.github.com>
2022-05-07 15:04:14 +0300

    Update MongoQuery.py

--- BSCScanAPI_MongoDB/MongoQuery.py
+++ BSCScanAPI_MongoDB/MongoQuery.py
@@ -13,3 +13,19 @@ def insertToPayable_Check(post):
 
 def insertToBNB_by_Block(post):
     collectionBNB.insert_one(post)
+
+def deleteFromPayable_Check(contractHash):
+    collectionPC.delete_one({'Contract_Hash': contractHash})
+
+def deleteFromBNB_by_Block(blockID):
+    collectionBNB.delete_one({'Block_ID': blockID})
+
+def deleteAll():
+    collectionPC.delete_many({})
+    collectionBNB.delete_many({})
+
+def findInPayable_Check(contractHash):
+    collectionPC.find({'Contract_Hash': contractHash})
+
+def findInBNB_by_Block(blockID):
+    collectionBNB.find({'Block_ID': blockID})
